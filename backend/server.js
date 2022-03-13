4675e2b1f53760c7f8621d7e4c078b2fa4f3b22c
hazedrops <hazedrops@hotmail.com>
2022-03-12 18:46:33 -0800

    userController & userRoutes files are added, starting set up the file structure

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+// Create a route
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'Welcome to the Support Desk API'})
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
